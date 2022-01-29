3913f61e470e08ee6b868424147313f506868cac
MatmanBJ <matmanbj@gmail.com>
2022-01-29 20:54:41 +0300

    beta 0_5

--- program/main.cpp
+++ program/main.cpp
@@ -1,16 +1,46 @@
 //
 //  main.cpp
 //  Alternative exam
-//  version beta 0.1
+//  version beta 0.5
 //  Created by Matthew Sobolewski on 27.12.2020.
 //
 
 #include <iostream>
 #include <stdlib.h>
+#include <cstdlib>
 #include <vector>
 #include <list>
 
 using namespace std;
+
+// VALUABLE CONSTANTS
+
+const int rows = 32; // general number of rows in array (number of items)
+const int columns = 10; // general number of columns in array (number of knapsacks)
+const int max_bin_sizes = 30; // maximal sizes for bins (days for workers)
+const int jobs_number = 32; // general number of jobs (translations)
+const int workers_number = 10; // general number of workers (translators)
+const int languages_number = 7; // general number of languages
+
+// FUNCTION PROTOTYPES
+
+char** simple_split(char *str, int length, char separator);
+int** input_data_file (int local_size, char* local_name);
+
+// INSTRUCTION
+
+// for jobs ("jobs.txt file"):
+// english - 1; german - 2; french - 3; spanish - 4; italian - 5; portugal - 6; polish - 7
+// second row - size
+// third row - complexity
+// fourth row - importance
+// fifth row - days
+
+// for workers:
+// Complexity matrix:
+// each row - worker, each column - language, "0" means "doesn't exist"
+// Speed matrix:
+// each row - worker, each column - language, "0" means "doesn't exist"
 
 int main()
 {
@@ -23,10 +53,6 @@ int main()
     int current_value;
     int current_row;
     int current_step;
-    
-    const int rows = 4; // general number of rows in array (number of items)
-    const int columns = 3; // general number of columns in array (number of knapsacks)
-    const int max_bin_sizes = 30;
     
     int isin[rows + 1][max_bin_sizes + 1];
     int isin_row[rows + 1][max_bin_sizes + 1];
@@ -54,13 +80,58 @@ int main()
         profit_function_modified_two[i] = new int[columns + 1];
     }
     
-    // VALUES FROM A HAND INPUT FOR OUR TASK
+    // BIN SIZES INITIALIZATION (DAYS FOR EACH WORKER)
+    
+    for (i = 0; i <= workers_number; i++) // it doesn't matter here, from "0" or "1" to start
+    {
+        bin_sizes[i] = max_bin_sizes;
+    }
+    
+    // VALUES FROM A AUTOMATIC FILE INPUT FOR OUR TASK
     
     int *translation_size = new int[rows + 1];
     int *translation_complexity = new int[rows + 1];
     int *translation_importance = new int[rows + 1];
     int *translation_days = new int[rows + 1];
-    float *translator_speed = new float[columns + 1];
+    float *translator_speed = new float[columns + 1]; // eto vsyo ne nado, mozho udalyat
+    
+    char jobs_file [] = {'j', 'o', 'b', 's', '.', 't', 'x', 't', '\0'};
+    char workers_c_file [] = {'w', 'o', 'r', 'k', 'e', 'r', 's', '_', 'c', '.', 't', 'x', 't', '\0'};
+    char workers_s_file [] = {'w', 'o', 'r', 'k', 'e', 'r', 's', '_', 's', '.', 't', 'x', 't', '\0'};
+    
+    int **jobs = input_data_file(jobs_number, jobs_file); // rows - values, columns - jobs
+    int **workers_c = input_data_file(languages_number, workers_c_file); // rows - workers, columns - languages
+    int **workers_s = input_data_file(languages_number, workers_s_file); // rows - workers, columns - languages
+    float **workers_s_float = new float*[workers_number + 1]; // rows - workers, columns - languages
+    
+    // TRANSLATION INTO THE FLOAT FORMAT FOR THE WORKERS' SPEED
+    
+    for (i = 1; i <= workers_number; i++)
+    {
+        workers_s_float[i] = new float[languages_number + 1];
+        for (j = 1; j <= languages_number; j++)
+        {
+            workers_s_float[i][j] = 8*(float)workers_s[i][j]/100;
+        }
+    }
+    
+    // FORMING PROFIT FUNCTION (ROWS - JOBS, COLUMNS - WORKERS)
+    
+    for (i = 1; i <= jobs_number; i++)
+    {
+        for (j = 1; j <= workers_number; j++)
+        {
+            if (workers_s[j][jobs[1][i]] != 0 && (float)jobs[2][i]/workers_s_float[j][jobs[1][i]] <= jobs[5][i])
+            {
+                item_sizes[i][j] = (int)((float)jobs[2][i]/workers_s_float[j][jobs[1][i]]);
+            }
+            else
+            {
+                item_sizes[i][j] = bin_sizes[j] + 1;
+            }
+            profit_function[i][j] = 1;
+        }
+    }
     
     translation_size[0] = 0;
     translation_size[1] = 5;
@@ -91,19 +162,62 @@ int main()
     translator_speed[2] = 0.5;
     translator_speed[3] = 1.5;
     
-    bin_sizes[0] = 0;
-    bin_sizes[1] = 30;
-    bin_sizes[2] = 30;
-    bin_sizes[3] = 30;
+    // JOBS VALUES OUTPUT
     
-    for (i = 1; i <= rows; i++)
+    cout << "Jobs values:" << endl;
+    for (i = 1; i <= 5; i++)
     {
-        for (j = 1; j <= columns; j++)
+        switch (i)
         {
-            item_sizes[i][j] = translation_size[i]/translator_speed[j];
-            profit_function[i][j] = item_sizes[i][j];
+            case 1:
+                cout << "Language:   ";
+                break;
+            case 2:
+                cout << "Lists:      ";
+                break;
+            case 3:
+                cout << "Compexity:  ";
+                break;
+            case 4:
+                cout << "Importance: ";
+                break;
+            case 5:
+                cout << "Days:       ";
+                break;
         }
+        for (j = 1; j <= jobs_number; j++)
+        {
+            cout << jobs[i][j] << " ";
+        }
+        cout << endl;
     }
+    cout << endl;
+    
+    // WORKERS COMPLEXITY VALUES OUTPUT
+    
+    cout << "Workers complexity:" << endl;
+    for (i = 1; i <= workers_number; i++)
+    {
+        for (j = 1; j <= languages_number; j++)
+        {
+            cout << workers_c[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    
+    // WORKERS SPEED VALUES OUTPUT
+    
+    cout << "Workers speed:" << endl;
+    for (i = 1; i <= workers_number; i++)
+    {
+        for (j = 1; j <= languages_number; j++)
+        {
+            cout << workers_s_float[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
     
     // VALUES FROM AN EXAMPLE HAND INPUT
     
@@ -173,6 +287,7 @@ int main()
     cout << "Item sizes:" << endl;
     for (i = 1; i <= rows; i++)
     {
+        cout << i << ":  ";
         for (j = 1; j <= columns; j++)
         {
             cout << item_sizes[i][j] << " ";
@@ -299,7 +414,7 @@ int main()
             }
         }
         
-        cout << "Profit function p^1: " << endl;
+        /*cout << "Profit function p^1: " << endl;
         for (i = 1; i <= rows; i++)
         {
             for (j = x; j <= columns; j++)
@@ -308,7 +423,7 @@ int main()
             }
             cout << endl;
         }
-        cout << endl;
+        cout << endl;*/
         
         for (i = 1; i <= rows; i++)
         {
@@ -318,7 +433,7 @@ int main()
             }
         }
         
-        cout << "Profit function p^2: " << endl;
+        /*cout << "Profit function p^2: " << endl;
         for (i = 1; i <= rows; i++)
         {
             for (j = x; j <= columns; j++)
@@ -327,11 +442,11 @@ int main()
             }
             cout << endl;
         }
-        cout << endl;
+        cout << endl;*/
         
         // TEMPORARY WORK ARRAY (A - KNAPSACK PROBLEM - ALGORITHM) OUTPUT
         
-        cout << "Temporary work array:" << endl;
+        /*cout << "Temporary work array:" << endl;
         for (i = 0; i <= rows; i++)
         {
             for (j = 0; j <= bin_sizes[x]; j++)
@@ -340,11 +455,11 @@ int main()
             }
             cout << endl;
         }
-        cout << endl;
+        cout << endl;*/
         
         // NUMBER OF CHOSEN ITEMS AT CURRENT ITERATION OUTPUT
         
-        cout << "Items number: ";
+        cout << "Step " << x << " items number: ";
         for (i = 0; i <= rows; i++)
         {
             if (S[i] == 1)
@@ -462,7 +577,7 @@ int main()
         
         // NUMBER OF CHOSEN ITEMS AT CURRENT ITERATION OUTPUT
         
-        cout << "Items number: ";
+        cout << "Step " << x << " items number: ";
         for (i = 0; i <= rows; i++)
         {
             if (S[i] == 1)
@@ -483,6 +598,18 @@ int main()
         delete [] profit_function_modified_two[i];
     }
     
+    for (i = 0; i <= 5; i++)
+    {
+        delete [] jobs[i];
+    }
+    
+    for (i = 0; i <= workers_number; i++)
+    {
+        delete [] workers_c[i];
+        delete [] workers_s[i];
+        delete [] workers_s_float[i];
+    }
+    
     delete [] bin_sizes;
     delete [] S;
     delete [] T;
@@ -492,5 +619,143 @@ int main()
     delete [] profit_function_modified_one;
     delete [] profit_function_modified_two;
     
+    delete [] jobs;
+    delete [] workers_c;
+    delete [] workers_s;
+    delete [] workers_s_float;
+    
     return 0;
+}
+
+char** simple_split(char *str, int length, char separator)
+{
+    char **string_array;
+    int i;
+    int j;
+    int k;
+    int m;
+    int key;
+    int count;
+    
+    string_array = nullptr;
+    
+    for (j = 0, m = 0; j < length; j++)
+    {
+        if (str[j] == separator)
+        {
+            m = m + 1;
+        }
+    }
+
+    key = 0;
+    string_array = new char*[m + 1];
+    if(string_array != nullptr)
+    {
+        for (i = 0, count = 0; i <= m; i++, count++)
+        {
+            string_array[i] = new char[length];
+            if (string_array[i] != nullptr)
+            {
+                key = 1;
+            }
+            else
+            {
+                key = 0;
+                i = m;
+            }
+        }
+        if (key == 1)
+        {
+            k = 0;
+            m = 0;
+            for (j = 0; j < length; j++)
+            {
+                if (str[j] != separator)
+                {
+                    string_array[m][j - k] = str[j];
+                }
+                else
+                {
+                    string_array[m][j - k] = '\0';
+                    k = j + 1;
+                    m = m + 1;
+                }
+            }
+        }
+        else
+        {
+            for (i = 0; i < count; i++)
+            {
+                delete [] string_array[i];
+            }
+            delete [] string_array;
+        }
+     }
+    return string_array;
+}
+
+int** input_data_file (int local_size, char* local_name)
+{
+    int i;
+    int j;
+    int n;
+    int local_length;
+    int **local_numbers;
+    FILE* local_file;
+    char local_separator;
+    char local_buffer[160];
+    char **local_buffer_two;
+    
+    i = 0;
+    j = 0;
+    n = 0;
+    local_length = 0;
+    local_numbers = nullptr;
+    local_file = nullptr;
+    local_separator = ';';
+    local_buffer_two = nullptr;
+    
+    //char local_name[160];
+    //fflush(stdin);
+    //printf("Please, print the name of the file: ");
+    //scanf("%s", local_name);
+    
+    local_file = fopen(local_name, "r");
+    
+    if (local_file != nullptr)
+    {
+        while (fgets(local_buffer, 160, local_file) != nullptr)
+        {
+            n = n + 1;
+        }
+        local_numbers = new int*[n + 1];
+        rewind(local_file);
+        for (i = 1; i <= n; i++)
+        {
+            fgets(local_buffer, 160, local_file);
+            local_length = (int)strlen(local_buffer);
+            local_buffer[local_length - 1] = '\0';
+            local_buffer_two = simple_split(local_buffer, local_length, local_separator);
+            if (local_buffer_two != nullptr)
+            {
+                local_numbers[i] = new int[local_size + 1];
+                for (j = 1; j <= local_size; j++)
+                {
+                    local_numbers[i][j] = atoi(local_buffer_two[j - 1]);
+                    delete [] local_buffer_two[j - 1];
+                }
+                delete [] local_buffer_two;
+            }
+            else
+            {
+                cout << "Error at data reading!\n";
+            }
+        }
+        fclose(local_file);
+    }
+    else
+    {
+        cout << "File hasn't been found!\n";
+    }
+    return local_numbers;
 }
